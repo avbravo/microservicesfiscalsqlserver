fcaeab73f7d7be4c3105df2e3042cdb4fd85b802
avbravo <avbravo@avbravo>
2019-11-19 21:48:38 -0500

    facturaResources

--- src/main/java/com/avbravo/microservicesfiscalsqlserver/JAXRSConfiguration.java
+++ src/main/java/com/avbravo/microservicesfiscalsqlserver/JAXRSConfiguration.java
@@ -1,5 +1,6 @@
 package com.avbravo.microservicesfiscalsqlserver;
 
+import java.util.Set;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
@@ -9,5 +10,11 @@ import javax.ws.rs.core.Application;
  */
 @ApplicationPath("resources")
 public class JAXRSConfiguration extends Application {
-    
+       @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> resources = new java.util.HashSet<>();
+        resources.add(com.avbravo.microservicesfiscalsqlserver.resources.FacturaResources.class);
+       
+             return resources;
+    }
 }
