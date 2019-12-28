83fa87c8603e1e8f6a57c8e460edc81acaf87706
avbravo <avbravo@avbravo>
2019-12-28 10:39:49 -0500

    buid finalname

--- src/main/java/com/avbravo/microservicesfiscalsqlserver/JAXRSConfiguration.java
+++ src/main/java/com/avbravo/microservicesfiscalsqlserver/JAXRSConfiguration.java
@@ -6,11 +6,13 @@ import javax.ws.rs.core.Application;
 
 /**
  * Configures JAX-RS for the application.
+ *
  * @author Juneau
  */
 @ApplicationPath("resources")
 public class JAXRSConfiguration extends Application {
-       @Override
+
+    @Override
     public Set<Class<?>> getClasses() {
         Set<Class<?>> resources = new java.util.HashSet<>();
         resources.add(com.avbravo.microservicesfiscalsqlserver.resources.FacturaResources.class);
@@ -20,7 +22,7 @@ public class JAXRSConfiguration extends Application {
         resources.add(com.avbravo.microservicesfiscalsqlserver.resources.TPedidosEnLineaResources.class);
         resources.add(com.avbravo.microservicesfiscalsqlserver.resources.TPedidosResources.class);
         resources.add(com.avbravo.microservicesfiscalsqlserver.resources.JavaEE8Resource.class);
-       
-             return resources;
+
+        return resources;
     }
 }
